8dd8ef9cc25bfb21a29e827ae027ab4709f680e1
Dasha Burgos <dasha.lewis@gmail.com>
2023-02-20 16:27:59 -0500

    added random photo generator and constructor function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// **** Globals ****
+let productsArray = [];
+let votingRounds = 25;
+
+
+// **** DOM Windows ****
+
+// **** Constructor Functions ****
+
+function renderImg(){
+  let imgOne = randomImg();
+  let imgTwo = randomImg();
+  let imgThree = randomImg();
+
+}
+
+function Products(name, fileExtension = '.jpg'){
+  this.name = name;
+  this.image = `img/${name}.${fileExtension}`;
+  this.votes = 0;
+  this.views = 0;
+}
+
+// **** Helper Functions / Utilities ***
+
+// get random image
+function randomImg(){
+  return Math.floor(Math.random() * productsArray.length);
+}
+
+// **** Executable Code ****
+
+
+
+
