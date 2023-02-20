913cc7864d37b2beeeb60480acd88bdc76cdab84
Dasha Burgos <dasha.lewis@gmail.com>
2023-02-20 16:50:03 -0500

    added constructor parameters

--- js/app.js
+++ js/app.js
@@ -7,14 +7,16 @@ let votingRounds = 25;
 
 // **** DOM Windows ****
 
-// **** Constructor Functions ****
+let imgContainer = document.getElementById('image-container');
 
-function renderImg(){
-  let imgOne = randomImg();
-  let imgTwo = randomImg();
-  let imgThree = randomImg();
+let firstImage = document.getElementById('imgOne');
+let secondImage = document.getElementById('imgTwo');
+let thirdImage = document.getElementById('imgThree');
 
-}
+
+
+
+// **** Constructor Function ****
 
 function Products(name, fileExtension = '.jpg'){
   this.name = name;
@@ -25,12 +27,44 @@ function Products(name, fileExtension = '.jpg'){
 
 // **** Helper Functions / Utilities ***
 
+function renderImg(){
+  let imgOne = randomImg();
+  let imgTwo = randomImg();
+  let imgThree = randomImg();
+  
+
+}
+
 // get random image
 function randomImg(){
   return Math.floor(Math.random() * productsArray.length);
 }
 
 // **** Executable Code ****
+
+let bag = new Products('bag');
+let banana = new Products('banana');
+let bathroom = new Products('bathroom');
+let boots = new Products('boots');
+let breakfast = new Products('breakfast');
+let bubblegum = new Products('bubblegum');
+let chair = new Products('chair');
+let cthulhu = new Products('cthulhu');
+let dogDuck = new Products('dogDuck');
+let dragon = new Products('dragon');
+let pen = new Products('pen');
+let petSweep = new Products('petSweep');
+let scissors = new Products('scissors');
+let shark = new Products('shark');
+let sweep = new Products('sweep');
+let tauntaun = new Products('tauntaun');
+let unicorn = new Products('unicorn');
+let waterCan = new Products('waterCan');
+let wineGlass = new Products('wineGlass');
+
+productsArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass);
+
+// renderImg();
 
 
 
